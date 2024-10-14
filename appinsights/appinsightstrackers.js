3e6e4d37d40cd3c41493843fbc92b849d55d5ffc
Lou Rainaldi <lourainaldi@microsoft.com>
2024-10-14 17:15:14 -0400

    Update appinsightstrackers.js
    refactoring and added enhancements with Tim

--- appinsights/appinsightstrackers.js
+++ appinsights/appinsightstrackers.js
@@ -12,6 +12,30 @@ function trackForm(Formname)
     this.appInsights.flush();
 }
 
+function whatEvent(executionContext)
+{
+    var eventSrcName;
+    if (executionContext != null) {
+            formContext = executionContext.getFormContext();
+            eventSrc = executionContext.getEventSource();
+        }
+ 
+        // identify the event source
+        if (eventSrc.tabs) {
+            eventSrcName = "ONLOAD";
+        }
+        else
+        {
+            // event source is a form, this is the "onload" event (tabs) or an onChange event from lgp_programid
+            // assign event handlers to all controls in the step set
+            eventSourceName = eventSrc.getName();
+//            for (var i = 0; i < count; i++) {
+//                // xxx
+//            }
+        }
+    return(eventSrc);
+}
+
 function trackMetric(executionContext,nameValue,metricValue)
 {
   var formContext = executionContext.getFormContext();
@@ -22,6 +46,11 @@ function trackMetric(executionContext,nameValue,metricValue)
 function trackEvent(executionContext, nameValue, propertiesDict)
 {
   var formContext = executionContext.getFormContext();
-  this.appInsights.trackEvent({name:nameValue, properties:propertiesDict})
+  var eventName = whatEvent(executionContext);
+  if (eventName != null)
+      this.appInsights.trackEvent({name:eventName, properties:propertiesDict})
+    else
+      this.appInsights.trackEvent({name:namevalue, properties:propertiesDict})
+    
   this.appInsights.flush();
 }
